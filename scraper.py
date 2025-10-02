17c67dc7761da91fdd61c9027922cd77fb19f275
jp <hannajohnpaul4@gmail.com>
2025-10-02 13:01:45 -0400

    got rid of the hardcode, should work for any headline aslong as the website syntax isnt changed

--- scraper.py
+++ scraper.py
@@ -3,30 +3,37 @@ from bs4 import BeautifulSoup
 from transformers import pipeline
 
 URL = "https://www.binghamton.edu/news/"
-story_url = "https://www.binghamton.edu/news/story/2024/05/30/binghamton-university-professor-receives-1-million-grant-to-study-mental-health-in-youth.html"
 
 print(f"fetching news from {URL}")
 try: 
     response = requests.get(URL)
-    story_response = requests.get(story_url)
-    print(f"response status code: {response.status_code}")
-
+    print(f"Status code:", {response.status_code})
     soup = BeautifulSoup(response.content, "html.parser")
-    story_soup = BeautifulSoup(story_response.content, "html.parser")
 
-    top_story_headline = soup.find("h2", class_= "headline")
+    heading_div = soup.find("section", id="feature-story")
 
-    if top_story_headline:
-        print("\nSUCCESS! Found the top story headline:")
-        top_story_headline_text = top_story_headline.text.strip()
-        print(top_story_headline_text)
-        
-        description = story_soup.find("div", id="story-body")
-        
-        if description:
-            description_text = description.text.strip()
-            print(f"Found description: {description_text}")
-            text_to_summarize = f"{top_story_headline_text}. {description_text}"
+    if heading_div:
+        article_link_tag = heading_div.find("a")
+        headline_tag = heading_div.find("h2", class_="headline")
+        if article_link_tag and headline_tag:
+            top_story_headline_text = headline_tag.text.strip()
+            print(f"\nSUCCESS! Found the top story headline: {top_story_headline_text}")
+            
+            partial_link = article_link_tag['href']
+            story_url = partial_link if partial_link.startswith("http") else f"https://www.binghamton.edu{partial_link}"
+            print("\n")
+            print(f"the story url is: {story_url}")
+            print("\n")
+            
+            story_response = requests.get(story_url)
+            story_soup = BeautifulSoup(story_response.content, "html.parser")
+            
+            description = story_soup.find("div", id="story-body")
+
+            if description:
+                description_text = description.text.strip()
+                print(f"Found description: \n\n{description_text[:500]}...")
+                text_to_summarize = f"{top_story_headline_text}. {description_text}"
     
     else:
             print("\nCould not find the headline. The website structure might have changed.")
